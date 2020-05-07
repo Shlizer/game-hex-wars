5b6a338e6f42dbbc4899a5e6e6fd2d5244ce3bfd
Krzysiek 'Shlizer' Hinc <hinc.krzysiek@gmail.com>
2020-05-07 23:52:45 +0200

    css variables

--- app/Client/index.tsx
+++ app/Client/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
 import TitleBar from './Titlebar';
 import Content from './Content';
+import './variables.global.css';
 import './main.global.css';
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
