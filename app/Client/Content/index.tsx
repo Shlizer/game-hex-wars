293ce4510b1dcf1251276233ff2cbe00df42b2ca
Krzysiek 'Shlizer' Hinc <hinc.krzysiek@gmail.com>
2020-05-15 16:41:30 +0200

    cursor - performance problems

--- app/Client/Content/index.tsx
+++ app/Client/Content/index.tsx
@@ -14,18 +14,25 @@ import CursorTest from './Cursor/test';
 import styles from './style.scss';
 import './style.global.scss';
 
+const customPointer = false;
+
 class Content extends React.Component {
   render() {
     return (
-      <div className={styles.content}>
+      <div
+        className={[
+          styles.content,
+          customPointer ? styles.customPointer : ''
+        ].join(' ')}
+      >
         {this.context && this.context.current.map ? <Canvas /> : <MapChoose />}
         <Loading />
         <ToastsContainer
           store={ToastsStore}
           position={ToastsContainerPosition.TOP_RIGHT}
         />
-        <Cursor />
-        <CursorTest />
+        {customPointer ? <Cursor /> : null}
+        {this.context && this.context.current.map ? null : <CursorTest />}
       </div>
     );
   }
