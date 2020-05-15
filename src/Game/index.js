11fc4b51a4b0e687d348d43400b774d4af56a743
Krzysiek 'Shlizer' Hinc <hinc.krzysiek@gmail.com>
2020-05-15 16:11:16 +0200

    remove old files

--- src/Game/index.js
+++
@@ -1,9 +0,0 @@
-import { Engine } from './engine.js'
-
-export function initialize(config) {
-    Engine.initialize(config)
-}
-
-export function run() {
-    window.requestAnimationFrame(Engine.run)
-}
